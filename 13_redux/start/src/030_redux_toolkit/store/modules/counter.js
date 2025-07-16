6680681d9821cef76e10cb2d8bab5508993e67c2
Asakawa Masaki <masakingu0303@gmail.com>
2025-07-17 03:12:29 +0900

    aa

--- 13_redux/start/src/030_redux_toolkit/store/modules/counter.js
+++ 13_redux/start/src/030_redux_toolkit/store/modules/counter.js
@@ -1,28 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const counter = createSlice({
+  name: 'counter',
+  initialState: 0,
+  reducers: {
+    add(state, { type, payload }) {
+      console.log(type, payload)
+      return state + payload;
+    },
+    minus(state, { type, payload }) {
+      console.log(type, payload)
+      return state - payload;
+    }
+  }
+})
+
+const { add, minus } = counter.actions;
+
 const initialState = 0;
 
-const reducer = (state = initialState, { type, payload }) => {
-  switch (type) {
-    case "counter/+":
-      return state + payload;
-    case "counter/-":
-      return state - payload;
-    default:
-      return state;
-  }
-};
+export { add, minus }
 
-const add = (payload) => {
-    return {
-        type: "counter/+",
-        payload
-    }
-}
-
-const minus = (payload) => {
-    return {
-        type: "counter/-",
-        payload
-    }
-}
-
-export { reducer, add, minus }
+export default counter.reducer
